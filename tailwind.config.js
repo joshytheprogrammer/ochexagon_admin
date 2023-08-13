133c4708dec5e267a72a680dd177a402cf7e7716
codayomide <smokayomide@gmail.com>
2023-08-13 15:46:29 +0100

    Adde custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,13 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    colors: {
+      'gray': '#D9D9D9',
+      'blueish-white': '#F7F7FB',
+      'primary-color': '#2C4094',
+      'black': '#000000',
+      'white': '#ffffff'
+    }
   },
   plugins: [],
 }
