e1b84e96d41821530c14d009e234630c2d11a731
codayomide <smokayomide@gmail.com>
2023-08-17 14:59:10 +0100

    Styling the message details page

--- components/adminPanel/dashboard/messages/MessageDetails.jsx
+++ components/adminPanel/dashboard/messages/MessageDetails.jsx
@@ -2,29 +2,29 @@ import { MdEmail } from "react-icons/md";
 
 const MessageDetails = ({ messageData }) => {
   return (
-    <>
+    <div className="h-full flex flex-col">
       {messageData ? (
         <>
-          <h1>{messageData.senderName + messageData.id}</h1>
+          <h1 className="text-[25px] font-bold">
+            {messageData.senderName + messageData.id}
+          </h1>
 
-          <div>
+          <div className="flex flex-row justify-between">
             <div>{messageData.email}</div>
             <div>{messageData.time}</div>
           </div>
 
           <div>{messageData.message}</div>
 
-          <div>
-            <button>
-              <MdEmail />
-              <span>Reply in Email</span>
-            </button>
-          </div>
+          <button className="bg-primary-color text-white self-end justify-self-end flex items-center">
+            <MdEmail />
+            <span>Reply in Email</span>
+          </button>
         </>
       ) : (
         <p>Message not found</p>
       )}
-    </>
+    </div>
   );
 };
 
