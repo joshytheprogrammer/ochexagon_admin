4885177e8af4c443bad8d4a9009714180ae2640b
codayomide <smokayomide@gmail.com>
2023-08-18 11:24:20 +0100

    Added the layout for all the pages in blog section

---
+++ app/(panel)/blog/layout.jsx
@@ -0,0 +1,13 @@
+const BlogPostsLayout = ({ children }) => {
+  return (
+    <div className="h-full w-full flex flex-col">
+      <h1 className="font-bold text-[25px]">Blog Section</h1>
+      
+      <div className="bg-white border w-full h-[85%] lg:h-[90%] rounded-lg mt-6 py-4 px-2 mo-sm:px-4 first-letter:self-center justify-self-center overflow-hidden">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default BlogPostsLayout;
