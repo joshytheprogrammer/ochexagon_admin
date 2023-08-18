b1c6df49a0fa03595360afdddd8769e85eb08866
codayomide <smokayomide@gmail.com>
2023-08-18 12:02:07 +0100

    Created custom array for blog posts

---
+++ utils/blogposts.js
@@ -0,0 +1,17 @@
+const blogPostSample = {
+  topic: "Lovely Weather Today",
+  email: "janedoe@example.com",
+  content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, velit quis inventore, minima laboriosam in nulla laborum necessitatibus ducimus, delectus nostrum. Possimus nostrum provident explicabo delectus amet aliquam, mollitia quaerat natus?",
+  created: "08/08/23",
+  lastModified: "18/08/23"
+};
+
+let count = 20;
+let blogPostArray = [];
+
+for (let i = 0; i < count; i++) {
+  const duplicatePosts = {...blogPostSample, id: i};
+  blogPostArray.push(duplicatePosts)
+}
+
+export default blogPostArray;
