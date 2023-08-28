19122a02e2f90357e9e0ad9a399dfb18aefb210f
codayomide <smokayomide@gmail.com>
2023-08-28 14:08:08 +0100

    Created the testimonials edit section

--- app/(panel)/testimonials/layout.jsx
+++ app/(panel)/testimonials/layout.jsx
@@ -1,7 +1,7 @@
 const TestimonialSectionLayout = ({ children }) => {
   return (
     <div className="h-full w-full flex flex-col">
-      <h1 className="font-bold text-[25px]">Blog Section</h1>
+      <h1 className="font-bold text-[25px]">Testimonials</h1>
       
       <div className="bg-white border w-full h-[85%] lg:h-[90%] rounded-lg mt-6 py-4 px-2 mo-sm:px-4 first-letter:self-center justify-self-center overflow-hidden">
         {children}
--- components/adminPanel/testimonials/TestimonialsEditComponent.jsx
+++ components/adminPanel/testimonials/TestimonialsEditComponent.jsx
@@ -1,28 +1,73 @@
-const TestimonialsEditComponent = ({ testimonialsData }) => {
+"use client";
+
+import Image from "next/image";
+import womanPicture from "@public/assets/woman.jpg";
+import { useForm } from "react-hook-form";
+
+const TestimonialsEditComponent = () => {
+  const { register, watch } = useForm();
+
+  const watchedValues = watch();
+
+  const requiredMsg = "This Field is required";
+
   return (
-    <div className="h-full">
-      <h1>Testimonial{testimonialsData.id}</h1>
-      <form action="">
+    <div className="h-full flex flex-row justify-between items-center">
+      <form action="" className="w-[40%]">
         <div className="input-container">
           <label htmlFor="">Name</label>
-          <input type="text" placeholder="Name" />
+          <input
+            type="text"
+            placeholder="Jane Doe"
+            {...register("name", { required: requiredMsg })}
+          />
         </div>
 
-        <div className="input-container-file">
+        <div className="input-container">
           <label htmlFor="">Profile Image</label>
-          <input type="file" accept="image*" />
+          <input
+            type="file"
+            accept="image*"
+            {...register("profileImg", { required: requiredMsg })}
+          />
         </div>
 
         <div className="input-container">
           <label htmlFor="">Location</label>
-          <input type="text" placeholder="Lagos, Nigeria" />
+          <input
+            type="text"
+            placeholder="Lagos, Nigeria"
+            {...register("location", { required: requiredMsg })}
+          />
         </div>
 
         <div className="input-container">
-          <label htmlFor="">Caption</label>
-          <textarea>Caption</textarea>
+          <label htmlFor="" className="font-bold">
+            Caption
+          </label>
+          <textarea
+            className="h-[150px]"
+            {...register("caption", { required: requiredMsg })}
+          ></textarea>
         </div>
       </form>
+
+      <div className="xl:p-8 xl:border-2 mb-8 lg:mb-4 drop-shadow-2xl rounded-2xl w-[48%] relative text-center xl:text-left h-fit">
+        <Image
+          src={watchedValues.profileImg && URL.createObjectURL(watchedValues.profileImg[0]) || womanPicture}
+          alt="Profile Picture"
+          width={60}
+          height={60}
+          className="absolute w-[60px] aspect-square -top-[24px] -left-[30px] border-2 rounded-full hidden xl:block"
+        />
+
+        <p className="max-w-full over lg:text-lg break-words">
+          {watchedValues.caption || "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus commodi ullam aspernatur! Officiis aliquid suscipit quibusdam unde quas doloremque vel voluptates quasi similique eius, voluptatum, hic ea. Distinctio, sed neque optio tempora odit laudantium a ad sit nostrum repudiandae! Ipsum?"}
+        </p>
+
+        <h3 className="text-xl font-bold mt-4">{"Jane Doe" || watchedValues.name}</h3>
+        <span className="font-semibold">{watchedValues.location || "Lagos, Nigeria"}</span>
+      </div>
     </div>
   );
 };
