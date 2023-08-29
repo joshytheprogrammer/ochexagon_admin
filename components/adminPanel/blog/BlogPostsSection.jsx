8c121e428cd4e11d5ba698d4064e969e09b343ab
codayomide <smokayomide@gmail.com>
2023-08-29 23:01:06 +0100

    Added search feature to blog posts section

--- components/adminPanel/blog/BlogPostsSection.jsx
+++ components/adminPanel/blog/BlogPostsSection.jsx
@@ -1,10 +1,18 @@
+"use client"
+
 import blogPostArray from "@utils/blogposts";
+import useSearch from "@utils/customHooks/useSearch";
 import Link from "next/link";
+import { useState } from "react";
 import { BiSolidPencil } from "react-icons/bi";
 import { HiOutlineSearch } from "react-icons/hi";
 
 const BlogSectionComponent = () => {
   const posts = blogPostArray;
+  const postTopics = 'topic' 
+  const [searchInput, setSearchInput] = useState('');
+
+  const { filteredData } = useSearch(posts, postTopics, searchInput);
 
   return (
     <div className="w-full h-full overflow-hidden">
@@ -15,7 +23,7 @@ const BlogSectionComponent = () => {
         </div>
 
         <div className="w-full mb-3 mo-lg:mb-0 mo-lg:w-[35%] relative">
-          <input type="text" className="search-bar w-full pl-[55px]" />
+          <input type="text" className="search-bar w-full pl-[55px]" onChange={(e) => setSearchInput(e.target.value)} />
           <HiOutlineSearch className="text-2xl absolute left-[20px] top-[6px] text-primary-color" />
         </div>
       </div>
@@ -24,16 +32,16 @@ const BlogSectionComponent = () => {
       <table className="table w-max mo-lg:w-full h-full text-left mo-lg:text-xl">
           <thead>
             <tr className="border-b-[1px] border-black">
-              <th className="pb-4 p-3">Last Modified</th>
+              <th className="pb-4 p-3">Actions</th>
               <th className="pb-4 p-3">Topic</th>
               <th className="pb-4 p-3">Author</th>
-              <th className="pb-4 p-3">Actions</th>
+              <th className="pb-4 p-3">Last Modified</th>
             </tr>
           </thead>
 
           <tbody className="w-full font-semibold">
-            {posts.map((post) => (
-              <tr key={post.id}>
+            {filteredData.map((post) => (
+              <tr key={post.id} className="h-fit">
                 <td className="text-[30px] flex p-3">
                   <Link href={`/blog/posts/${post.id}`} className="text-primary-color mr-3">
                     <BiSolidPencil />
@@ -42,9 +50,9 @@ const BlogSectionComponent = () => {
                     <MdDelete />
                   </button> */}
                 </td>
-                <td className="p-3">{post.topic}</td>
-                <td className="p-3">{post.author}</td>
-                <td className="p-3">{post.lastModified}</td>
+                <td className="p-3 h-fit">{post.topic}</td>
+                <td className="p-3 h-fit">{post.author}</td>
+                <td className="p-3 h-fit">{post.lastModified}</td>
               </tr>
             ))}
           </tbody>
