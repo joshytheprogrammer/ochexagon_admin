e8644a58c9f038289e06ca07b75f702a1e9d1e4d
codayomide <smokayomide@gmail.com>
2023-08-30 14:44:35 +0100

    Added search feature to testimonials section

--- utils/testimonials.js
+++ utils/testimonials.js
@@ -1,6 +1,6 @@
 const testimonialSample = {
   testifier: "Jane Doe",
-  caption: "Nice Products",
+  captionTemplate: "Nice Products",
   content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, velit quis inventore, minima laboriosam in nulla laborum necessitatibus ducimus, delectus nostrum. Possimus nostrum provident explicabo delectus amet aliquam, mollitia quaerat natus?",
   created: "08/08/23",
   lastModified: "18/08/23"
@@ -10,7 +10,8 @@ let count = 20;
 let testimonialsArray = [];
 
 for (let i = 0; i < count; i++) {
-  const duplicateTestimonials = {...testimonialSample, id: i};
+  const testimonialCaption = testimonialSample.captionTemplate + ' ' + i;
+  const duplicateTestimonials = {...testimonialSample, id: i, caption: testimonialCaption};
   testimonialsArray.push(duplicateTestimonials)
 }
 
