ca4b37633f30cc28c7afaa7d3b441a546bff751b
codayomide <smokayomide@gmail.com>
2023-08-31 20:05:50 +0100

    Minor change

--- app/(panel)/dashboard/layout.jsx
+++ app/(panel)/dashboard/layout.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import MessagesContext from "@utils/context/MessagesContext";
 import { fetchMessages } from "@utils/firebase/utils";
@@ -23,7 +23,7 @@ const DashboardLayout = ({ children }) => {
     <MessagesContext.Provider value={messages}>
       <div className="h-full w-full flex flex-col">
         <h1 className="font-bold text-[25px]">Messages</h1>
-      
+
         <div className="bg-white border w-full h-[85%] lg:h-[90%] rounded-lg mt-6 py-4 px-2 mo-sm:px-4 first-letter:self-center justify-self-center overflow-hidden">
           {children}
         </div>
