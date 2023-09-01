9bb99042033b2696a2ab88612cfad779d882178e
codayomide <smokayomide@gmail.com>
2023-09-01 14:11:15 +0100

    Converted timestamp to jsDate

--- components/adminPanel/dashboard/Messages.jsx
+++ components/adminPanel/dashboard/Messages.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import MessagesContext from "@utils/context/MessagesContext";
 import Link from "next/link";
@@ -6,29 +6,68 @@ import { useContext } from "react";
 
 const Messages = () => {
   const messagesList = useContext(MessagesContext);
+
+  const unReadMessages = messagesList.filter((msg) => msg.data.isRead == false);
+
+  const toDateTime = (timestamp) => {
+    const milliseconds = (timestamp.seconds * 1000) + (timestamp.nanoseconds / 1000000);
   
+    const jsDate = new Date(milliseconds);
+  
+    return jsDate;
+  }
+
+
+  // messagesList.forEach((message) => {
+  //   const firebaseDate = new Date(
+  //     message.data.timestamp.seconds * 1000 + message.data.timestamp.nanoseconds / 1000000
+  //   );
+
+  //   const year = firebaseDate.getFullYear();
+  //   const month = firebaseDate.getMonth() + 1;
+  //   const day = firebaseDate.getDate();
+  //   const hours = firebaseDate.getHours();
+  //   const minutes = firebaseDate.getMinutes();
+  //   const seconds = firebaseDate.getSeconds();
+
+  //   console.log(hours)
+  // });
+
   return (
     <div className="h-full overflow-hidden flex flex-col">
       <div className="flex flex-col-reverse mo-lg:flex-row justify-between mb-2 lg:mb-0">
-        <span className="datalength-btn">{messagesList.length == 1 ? `${messagesList.length} message` : `${messagesList.length} messages`}</span>
-        <input type="text" className="search-btn w-full mo-lg:w-[50%] mb-5 lg:mb-0" />
+        <span className="datalength-btn">
+          {messagesList.length == 1
+            ? `${messagesList.length} message`
+            : `${messagesList.length} messages`}
+        </span>
+        <input
+          type="text"
+          className="search-btn w-full mo-lg:w-[50%] mb-5 lg:mb-0"
+        />
       </div>
 
-      <p className="text-sm font-semibold my-2">3 Unread Messages</p>
+      <p className="text-sm font-semibold my-2">{`${unReadMessages.length} Unread Messages`}</p>
 
       <hr />
 
-      <div className="scrollable-content overflow-y-scroll h-[100%] my-4 px-2">
+      <div className="scrollable-content overflow-y-auto h-[100%] my-4 px-2">
         {messagesList.map((message, index) => (
-          <Link href={`dashboard/messages/${message.id}`} key={index} className="w-full block py-2 hover:bg-transparent-blue hover:border-l-4 border-primary-color mb-1 last:mb-0 lg:px-4 rounded-[4px]">
+          <Link
+            href={`dashboard/messages/${message.id}`}
+            key={index}
+            className="w-full block py-2 hover:bg-transparent-blue hover:border-l-4 border-primary-color mb-1 last:mb-0 lg:px-4 rounded-[4px]"
+          >
             <div className="font-bold flex justify-between mb-2">
               <h2 className="text-lg">{message.data.senderName}</h2>
               <span className="text-sm">{message.data.email}</span>
             </div>
 
             <div className="font-semibold text-sm flex justify-between">
-              <p className="truncate-text w-[75%]">{message.data.messageSent}</p>
-              <span>18:47</span>
+              <p className="truncate-text w-[75%]">
+                {message.data.messageSent}
+              </p>
+              <span>{`${toDateTime(message.data.timestamp).getHours()}:${toDateTime(message.data.timestamp).getMinutes()}`}</span>
             </div>
           </Link>
         ))}
