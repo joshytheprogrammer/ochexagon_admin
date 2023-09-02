6d025d36b5c845eed1f14e53efc06f777442d477
codayomide <smokayomide@gmail.com>
2023-09-02 14:10:55 +0100

    Worked on realtime updates

--- components/adminPanel/dashboard/Messages.jsx
+++ components/adminPanel/dashboard/Messages.jsx
@@ -2,14 +2,49 @@
 
 import MessagesContext from "@utils/context/MessagesContext";
 import Link from "next/link";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
+import {
+  doc,
+  updateDoc,
+  onSnapshot,
+  collection,
+  query,
+  orderBy,
+} from "firebase/firestore";
+import { firestore } from "@utils/firebase/firebase";
 
 const Messages = () => {
-  const messagesList = useContext(MessagesContext);
+  // const messagesList = useContext(MessagesContext);
 
-  const unReadMessages = messagesList.filter((msg) => msg.data.isRead == false);
+  const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  function displayDateOrTime(timestamp) {
+  const colRef = collection(firestore, "messages");
+  const q = query(colRef, orderBy("timestamp", "desc"));
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const updatedData = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setMessages(updatedData);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, [q]);
+
+  const unReadMessages = messages.filter((msg) => msg.isRead == false);
+
+  const handleClick = async (messageId) => {
+    const messageRef = doc(firestore, "messages", messageId);
+    await updateDoc(messageRef, {
+      isRead: true,
+    });
+  };
+
+  const displayDateOrTime = (timestamp) => {
     const toDateTime = (timestamp) => {
       const milliseconds =
         timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000;
@@ -36,15 +71,15 @@ const Messages = () => {
       const year = inputDate.getFullYear().toString();
       return `${day}/${month}/${year}`;
     }
-  }
+  };
 
   return (
     <div className="h-full overflow-hidden flex flex-col">
       <div className="flex flex-col-reverse mo-lg:flex-row justify-between mb-2 lg:mb-0">
         <span className="datalength-btn">
-          {messagesList.length == 1
-            ? `${messagesList.length} message`
-            : `${messagesList.length} messages`}
+          {messages.length == 1
+            ? `${messages.length} message`
+            : `${messages.length} messages`}
         </span>
         <input
           type="text"
@@ -52,31 +87,39 @@ const Messages = () => {
         />
       </div>
 
-      <p className="text-sm font-semibold my-2">{`${unReadMessages.length} Unread Messages`}</p>
+      <p className="text-sm font-semibold my-2">
+        {unReadMessages.length == 1
+          ? `${unReadMessages.length} Unread message`
+          : `${unReadMessages.length} Unread messages`}
+      </p>
 
       <hr />
 
-      <div className="scrollable-content overflow-y-auto h-[100%] my-4 px-2">
-        {messagesList.map((message, index) => (
-          <Link
-            href={`dashboard/messages/${message.id}`}
-            key={index}
-            className="w-full block py-2 hover:bg-transparent-blue hover:border-l-4 border-primary-color mb-1 last:mb-0 lg:px-4 rounded-[4px]"
-          >
-            <div className="font-bold flex justify-between mb-2">
-              <h2 className="text-lg">{message.data.senderName}</h2>
-              <span className="text-sm">{message.data.email}</span>
-            </div>
+      {loading && <div>Loading...</div>}
+      {messages && (
+        <div className="scrollable-content overflow-y-auto h-[100%] my-4 px-2">
+          {messages.map((message, index) => (
+            <Link
+              href={`dashboard/messages/${message.id}`}
+              onClick={() => handleClick(message.id)}
+              key={index}
+              className={`messageLink w-full block py-2 hover:bg-transparent-blue hover:border-l-4 border-primary-color mb-1 last:mb-0 lg:px-4 rounded-[4px] ${
+                message.isRead ? "read" : "unread"
+              }`}
+            >
+              <div className="flex justify-between mb-2">
+                <h2 className="text-lg">{message.senderName}</h2>
+                <span className="text-sm">{message.email}</span>
+              </div>
 
-            <div className="font-semibold text-sm flex justify-between">
-              <p className="truncate-text w-[75%]">
-                {message.data.messageSent}
-              </p>
-              <span>{`${displayDateOrTime(message.data.timestamp)}`}</span>
-            </div>
-          </Link>
-        ))}
-      </div>
+              <div className="text-sm flex justify-between">
+                <p className="truncate-text w-[75%]">{message.messageSent}</p>
+                <span>{`${displayDateOrTime(message.timestamp)}`}</span>
+              </div>
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
