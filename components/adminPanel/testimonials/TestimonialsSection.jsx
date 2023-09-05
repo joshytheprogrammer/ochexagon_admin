b1c714224dfecd3691e99e63e5c87690fe1a802f
codayomide <smokayomide@gmail.com>
2023-09-05 12:29:06 +0100

    Worked on fetching testimonials data

--- components/adminPanel/testimonials/TestimonialsSection.jsx
+++ components/adminPanel/testimonials/TestimonialsSection.jsx
@@ -1,18 +1,21 @@
 "use client"
 
+import TestimonialsContext from "@utils/context/TestimonialsContext";
 import useSearch from "@utils/customHooks/useSearch";
 import testimonialsArray from "@utils/lists/testimonials";
 import Link from "next/link";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { BiSolidPencil } from "react-icons/bi";
 import { HiOutlineSearch } from "react-icons/hi";
 
 const TestimonialsComponent = () => {
-  const testimonials = testimonialsArray;
-  const testimonialsCaption = 'caption';
+  // const testimonials = testimonialsArray;
+  // const testimonialsCaption = 'caption';
   const [searchInput, setSearchInput] = useState('');
 
-  const { filteredData } = useSearch(testimonials, testimonialsCaption, searchInput);
+  // const { filteredData } = useSearch(testimonials, testimonialsCaption, searchInput);
+
+  const { testimonials, loading } = useContext(TestimonialsContext);
 
   return (
     <div className="w-full h-full overflow-hidden">
@@ -29,27 +32,27 @@ const TestimonialsComponent = () => {
       </div>
 
       <div className="h-[75%] mo-lg:h-[90%] scrollable-content overflow-auto">
-        <table className="table w-max mo-lg:w-full h-fit text-left mo-lg:text-xl">
+        <table className="table w-max mo-lg:w-full h-fit text-left mo-lg:text-md">
           <thead>
             <tr className="border-b-[1px] border-black">
               <th className="pb-4 p-3">Actions</th>
-              <th className="pb-4 p-3">Caption</th>
+              <th className="pb-4 p-3 w-[60%] truncate-text">Caption</th>
               <th className="pb-4 p-3">Testifier</th>
               <th className="pb-4 p-3">Last Modified</th>
             </tr>
           </thead>
 
-          {filteredData.length == 0 ? <div>No result found</div> : <tbody className="w-full font-semibold">
-            {filteredData.map((testimonial) => (
+          {testimonials.length == 0 ? <div>No result found</div> : <tbody className="w-full font-semibold">
+            {testimonials.map((testimonial) => (
               <tr key={testimonial.id}>
                 <td className="text-[30px] flex p-3">
                   <Link href={`testimonials/${testimonial.id}`} className="text-primary-color mr-3">
                     <BiSolidPencil />
                   </Link>
                 </td>
-                <td className="p-3">{testimonial.caption}</td>
-                <td className="p-3">{testimonial.testifier}</td>
-                <td className="p-3">{testimonial.lastModified}</td>
+                <td className="p-3">{testimonial.data.testimony}</td>
+                <td className="p-3">{testimonial.data.name}</td>
+                <td className="p-3"></td>
               </tr>
             ))}
           </tbody>}
