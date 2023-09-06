49a21680a0c3d7b61ef0505cc19ca43325d947ae
codayomide <smokayomide@gmail.com>
2023-09-06 09:57:29 +0100

    Added delete function

--- components/adminPanel/blog/posts/BlogEditComponent.jsx
+++ components/adminPanel/blog/posts/BlogEditComponent.jsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { firestore, storage } from "@utils/firebase/firebase";
-import { doc, updateDoc } from "firebase/firestore";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import Modal from "react-modal";
+import { FaTrashAlt } from "react-icons/fa";
+import { useState } from "react";
 
 const BlogEditComponent = ({ blogData }) => {
   const adjustTextareaHeight = (textarea) => {
@@ -13,6 +15,8 @@ const BlogEditComponent = ({ blogData }) => {
   };
 
   const router = useRouter();
+
+  const [deleteIsOpen, setDeleteIsOpen] = useState(false);
 
   const {
     register,
@@ -24,6 +28,11 @@ const BlogEditComponent = ({ blogData }) => {
     router.push("/blog");
     router.refresh();
   };
+
+  const deleteBlog = async () => {
+    redirect();
+    await deleteDoc(doc(firestore, "blog", blogData.id));
+  }
 
   const onSubmit = async (data) => {
     try {
@@ -64,7 +73,10 @@ const BlogEditComponent = ({ blogData }) => {
           className="w-full h-full flex flex-col-reverse sm:flex-col pr-2 overflow-auto scrollable-content"
           onSubmit={handleSubmit(onSubmit)}
         >
-          <div className="w-full flex justify-end mb-12">
+          <div className="w-full flex justify-between mb-12">
+            <div className="text-white flex items-center justify-center cursor-pointer bg-red text-lg rounded-md mr-8 px-3 py-1" onClick={() => setDeleteIsOpen(true)}>
+              <FaTrashAlt />
+            </div>
             <input
               type="submit"
               value={isSubmitting ? "Saving..." : "Save"}
@@ -74,6 +86,7 @@ const BlogEditComponent = ({ blogData }) => {
               }`}
             />
           </div>
+
           <div>
             <div className="flex flex-col sm:flex-row sm:justify-between sm:mb-8">
               <div className="input-container w-full sm:w-[45%]">
@@ -136,6 +149,29 @@ const BlogEditComponent = ({ blogData }) => {
           >
             View all Blogs
           </button>
+        </div>
+      </Modal>
+
+      <Modal
+        isOpen={deleteIsOpen}
+        className="bg-white w-[30%] rounded-lg"
+        overlayClassName="bg-primary-color bg-opacity-20 flex justify-center items-center absolute top-0 bottom-0 right-0 left-0"
+      >
+        <div className="px-4 py-6">
+          <div className="mb-12">
+            <h2 className="font-bold text-lg">Are you sure you want to delete?</h2>
+            <p>Your Blog will be deleted from the database</p>
+          </div>
+          <div className="flex justify-end">
+            <button className="bg-primary-color text-white px-4 py-2 rounded-lg mr-2" onClick={() => setDeleteIsOpen(false)}>Cancel</button>
+            <button
+              type="button"
+              className="bg-red text-white px-4 py-2 rounded-lg"
+              onClick={deleteBlog}
+            >
+              <FaTrashAlt />
+            </button>
+          </div>
         </div>
       </Modal>
     </div>
