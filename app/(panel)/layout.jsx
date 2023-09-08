4f84d72cd9daa7edf95a7198274b1c8ca9b15646
codayomide <smokayomide@gmail.com>
2023-09-08 12:34:12 +0100

    Worked on protecting routes

--- app/(panel)/layout.jsx
+++ app/(panel)/layout.jsx
@@ -5,6 +5,7 @@ import MenuBtn from "@components/adminPanel/MenuBtn";
 import SidePanel from "@components/adminPanel/SidePanel";
 import { auth } from "@utils/firebase/firebase";
 import { onAuthStateChanged } from "firebase/auth";
+import Link from "next/link";
 // import { useRouter } from "next/navigation";
 import { useState } from "react";
 
@@ -15,36 +16,45 @@ const AdminPanelLayout = ({ children }) => {
     !isOpen && setIsOpen(true);
   };
 
-  // const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   // const router = useRouter();
-
 
   onAuthStateChanged(auth, (user) => {
     if (user) {
-      console.log('You are signed in');
+      console.log("You are signed in");
+      setIsAuthenticated(true);
     } else {
       // router.push('/')
-      console.log('You need to sign in')
+      console.log("You need to sign in");
+      setIsAuthenticated(false);
     }
-  })
+  });
 
   // const user = auth.currentUser;
 
   // if (!user) {
   //   router.push("/");
   // } else {
-    return (
-      <div className="adminPanel bg-blueish-white w-full flex flex-col h-screen">
-        <Header />
-        <div className="flex-1 flex overflow-hidden">
-          <SidePanel isOpen={isOpen} setIsOpen={setIsOpen} />
-          <main className="w-full lg:w-[85%] h-full px-4 mo-lg:px-8 lg:px-12 py-4">
-            <MenuBtn onClick={openMenu} />
-            {children}
-          </main>
+  return (
+    <div>
+      {isAuthenticated ? (
+        <div className="adminPanel bg-blueish-white w-full flex flex-col h-screen">
+          <Header />
+          <div className="flex-1 flex overflow-hidden">
+            <SidePanel isOpen={isOpen} setIsOpen={setIsOpen} />
+            <main className="w-full lg:w-[85%] h-full px-4 mo-lg:px-8 lg:px-12 py-4">
+              <MenuBtn onClick={openMenu} />
+              {children}
+            </main>
+          </div>
         </div>
-      </div>
-    );
+      ) : (
+        <div>
+          You are not yet signed in! <Link href="/" className="text-primary-color underline">Go to login page</Link>
+        </div>
+      )}
+    </div>
+  );
   // }
 };
 
