871804689c42e63618c9b802d3abc285efa7b26f
codayomide <smokayomide@gmail.com>
2023-09-08 13:25:30 +0100

    Small change

--- components/login/notSignedIn/NotSignedIn.jsx
+++ components/login/notSignedIn/NotSignedIn.jsx
@@ -5,7 +5,7 @@ const NotSignedIn = () => {
     <div className="w-full h-screen bg-gray flex flex-col justify-center items-center">
       <div className=" w-[95%] mo-sm:w-[85%] mo-md:w-[75%] sm:w-[60%] md:w-[50%] lg:w-[45%] xl:w-[25%] rounded-lg bg-white flex flex-col p-6">
         <h1 className="font-bold text-xl mb-[50px]">You are not yet signed in!</h1>
-        <Link href="/" className="bg-primary-color text-white px-4 py-2 text-center rounded-[4px]">
+        <Link href="/" className="bg-primary-color text-white px-4 py-2 text-center rounded-[4px] font-semibold">
           Go to login page
         </Link>
       </div>
